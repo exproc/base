4d4e3b5d3365fdf9a47b1a5f58e66e3f9787e3a5
Trator Administrator <administrator@cchalifo.xyz>
2023-05-19 15:15:05 -0400

    modified:   root/start.sh

--- root/start.sh
+++ root/start.sh
@@ -5,15 +5,6 @@ PGID=${PGID:-6000}
 groupmod -o -g "$PGID" abc
 usermod -o -u "$PUID" abc
 
-echo '
-───────────────────────────────────────
-GID/UID
-───────────────────────────────────────'
-echo "
-User UID:    $(id -u abc)
-User GID:    $(id -g abc)
-───────────────────────────────────────
-"
 
 supervisord -c  /etc/supervisord.conf
 exec "$@"
